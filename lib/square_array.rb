5606610cbcaff0a6629a740d8ae374aec1bbfaad
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-30 20:52:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,7 @@
-array = [ 1, 2, 3, 4, 5]
-
+array = [ 1, 2, 3]
+count = 0
 
 def square_array(array)
-  count = 0
   squared_array = Array.new
   while count < array.length do
     squared_array << array[index ** 2]
