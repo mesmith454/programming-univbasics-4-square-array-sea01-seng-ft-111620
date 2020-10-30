d440545d82e0775c8fbd51b9964ec287a10f4025
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-30 21:20:39 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,10 @@
-array = [1, 2, 3]
+array = [ 1, 2, 3]
 
 def square_array(array)
-  count = 0
-  new_numbers = []
-  while count < array.length
-    array.length do |i|
-      array.each { new_numbers << Math.sqrt(i) }
-    end
-  count += 1 
-  end
-  puts new_numbers
+  array.length.times {  
+    Math.sqrt(array[i])
+    new_array.push
+  }
+  squared_array
 end
+
