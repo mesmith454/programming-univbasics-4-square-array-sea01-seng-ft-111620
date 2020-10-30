bd1b6fb1408f9f936fb6597a5844a4edb1e3039d
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-30 21:13:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,8 @@ array = [ 1, 2, 3]
 
 def square_array(array, index)
   count = 0
-  length = array.length
   squared_array = Array.new
-  while count < length do
+  while count < array.length do |index|
     squared_array << array[index] ** 2
     count += 1
   end 
