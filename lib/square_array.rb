8f297c8eb9d45b06f5a72363720473f5578a7c73
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-30 20:40:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,9 @@
-array = [1, 2, 3]
 
 def square_array(array)
-  count = 0
-  square = [i] ** 2
-  new_numbers = [] #empty array
-  while count < array.length do
-      new_numbers << square
-    count += 1
+  squared_array = []
+  array.each do |num|
+    square = num ** 2
+    squared_array << square
   end
+  squared_array
 end
