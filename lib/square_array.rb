2d2712549af7842d3fe945069ce79d7eb0b05cee
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-30 20:35:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   count = 0
   new_numbers = [] #empty array
   while count < array.length do
-      array { new_numbers << Math.sqrt(i) } 
+      array { new_numbers << i ** 2 } 
     count += 1
   end
   return new_numbers 
